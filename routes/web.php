419ad7dd181a141ecf62252de79e1d0f0c844833
JESÚS ALBERTO CANUL ACOSTA <safeequipment506@gmail.com>
2021-04-22 14:09:02 -0500

    Crud Recursos
    Se realizo el crud de recursos

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,12 @@ Route::get('/', function () {
 //Zona Vistas Director de Carreras
 Route::view('espacios', 'directorCarreras.espacios');
 Route::view('solicitudes', 'directorCarreras.solicitudes');
+Route::view('recursos', 'directorCarreras.recursos');
 
 //Zona de APIS
 Route::apiResource('apiSolicitudes', 'App\Http\Controllers\SolicitudesController');
 Route::apiResource('apiEspacios', 'App\Http\Controllers\EspaciosController');
+Route::apiResource('apiRecurso', 'App\Http\Controllers\ApiRecursoController');
 
 //Envio de Email
 Route::POST('contact', 'App\Http\Controllers\EnviarEmailController@mensaje');
