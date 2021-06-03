97e0a15b5a1492f6ed2b4ccdf32b06939d1a0b04
RomanCan <romancan33@gmail.com>
2021-06-03 15:01:18 -0500

    Revert "bug de correo"
    This reverts commit a42dfa13cd0b85afe618bad3a414183fa0d7a861.

--- public/js/director/usuarios.js
+++ public/js/director/usuarios.js
@@ -1,5 +1,5 @@
 var route = document.querySelector("[name=route]").value;
-var UrlUsuarios = route + '/apiUsuarios';
+var UrlUsuarios = route +'/apiUsuarios';
 var UrlDocentes = route + '/apiDocentes';
 var UserName = route + '/username';
 new Vue({
@@ -10,157 +10,157 @@ new Vue({
     },
     el: '#usuario',
     data: {
-        docentes: [],
+        docentes:[],
         usuarios: [],
 
-        cedula: '',
-        i_cedula: '',
+        cedula:'',
+        i_cedula:'',
 
-        id_rol: '',
-        nombre: '',
-        apellidop: '',
-        apellidom: '',
-        nivelestudio: '',
-        profesion: '',
-        tratamiento: '',
-        activo: '',
-        foto: '',
-        usuario: '',
-        password: '',
-        email: '',
+        id_rol:'',
+        nombre:'',
+        apellidop:'',
+        apellidom:'',
+        nivelestudio:'',
+        profesion:'',
+        tratamiento:'',
+        activo:'',
+        foto:'',
+        usuario:'',
+        password:'',
+        email:'',
 
-        editar: false,
-        editarInfo: false,
+        editar:false,
+        editarInfo:false,
     },
     created() {
         this.getUsuarios();
         this.getDocentes();
     },
     methods: {
-        getUsuarios: function() {
+        getUsuarios:function(){
             this.$http.get(UrlUsuarios)
-                .then(function(json) {
-                    this.usuarios = json.data;
-                    console.log(json);
-                })
+            .then(function(json){
+                this.usuarios = json.data;
+                console.log(json);
+            })
         },
-        getDocentes: function() {
+        getDocentes:function(){
             this.$http.get(UrlDocentes)
-                .then(function(json) {
-                    this.docentes = json.data;
-                })
+            .then(function(json){
+                this.docentes = json.data;
+            })
         },
-        showModal: function() {
+        showModal:function(){
             $('#agregar_user').modal('show');
         },
-        editarI: function(id) {
+        editarI:function(id){
             this.$http.get(UrlDocentes + '/' + id)
-                .then(function(json) {
-                    this.id_rol = json.data.id_rol;
-                    this.nombre = json.data.nombre;
-                    this.apellidop = json.data.apellidop;
-                    this.apellidom = json.data.apellidom;
-                    this.nivelestudio = json.data.nivelestudio;
-                    this.profesion = json.data.profesion;
-                    this.tratamiento = json.data.tratamiento;
-                    this.activo = json.data.activo;
-                    this.foto = json.data.foto;
-                    this.usuario = json.data.usuario;
-                    this.password = json.data.password;
-                    this.email = json.data.email;
-                    this.i_cedula = json.data.cedula;
-                    this.editarInfo = true;
-                })
+            .then(function(json){
+                this.id_rol = json.data.id_rol;
+                this.nombre = json.data.nombre;
+                this.apellidop = json.data.apellidop;
+                this.apellidom = json.data.apellidom;
+                this.nivelestudio = json.data.nivelestudio;
+                this.profesion = json.data.profesion;
+                this.tratamiento = json.data.tratamiento;
+                this.activo = json.data.activo;
+                this.foto = json.data.foto;
+                this.usuario = json.data.usuario;
+                this.password = json.data.password;
+                this.email = json.data.email;
+                this.i_cedula = json.data.cedula;
+                this.editarInfo = true;
+            })
         },
-        guardar: function() {
-            var doc = {
-                id_rol: this.id_rol,
-                nombre: this.nombre,
-                apellidop: this.apellidop,
-                apellidom: this.apellidom,
-                nivelestudio: this.nivelestudio,
-                profesion: this.profesion,
-                tratamiento: this.tratamiento,
-                activo: this.activo,
-                foto: this.foto,
-                usuario: this.usuario,
-                password: this.password,
-                email: this.email,
+        guardar:function(){
+            var doc ={
+                id_rol : this.id_rol,
+                nombre : this.nombre,
+                apellidop : this.apellidop,
+                apellidom : this.apellidom,
+                nivelestudio : this.nivelestudio,
+                profesion : this.profesion,
+                tratamiento : this.tratamiento,
+                activo : this.activo,
+                foto : this.foto,
+                usuario : this.usuario,
+                password : this.password,
+                email : this.email,
 
             };
 
             Swal.fire({
-                title: "No podrás revertir este cambio!,¿Estás seguro?",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, guardar',
-                cancelButtonText: 'No, cancelar',
-            }).then((result) => {
-                if (result.value) {
-                    this.$http.patch(UrlDocentes + '/' + this.i_cedula, doc)
-                        .then(function(json) {
-                            Swal.fire({
-                                position: 'center',
-                                icon: 'success',
-                                title: '¡Guardado exitosamente!',
-                                showConfirmButton: false,
-                                timer: 1500
-                            })
+				title: "No podrás revertir este cambio!,¿Estás seguro?",
+				icon: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#3085d6',
+				cancelButtonColor: '#d33',
+				confirmButtonText: 'Sí, guardar',
+				cancelButtonText:'No, cancelar',
+			}).then((result)=>{
+                if(result.value){
+                    this.$http.patch(UrlDocentes +'/'+this.i_cedula,doc)
+                    .then(function(json){
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'success',
+                            title: '¡Guardado exitosamente!',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
                             this.enviarEmail();
                             this.salir();
                             window.location.reload();
-                            //
+                        //
 
-                            // this.getUsuarios();
-                            // window.location.reload();
-                        }).catch(function() {
-                            Swal.fire({
-                                icon: 'error',
-                                title: '¡Ha ocurrido un error!',
-                                text: '¡No deje campos vacios!',
-                            })
+                        // this.getUsuarios();
+                        // window.location.reload();
+                    }).catch(function(){
+                        Swal.fire({
+                            icon: 'error',
+                            title: '¡Ha ocurrido un error!',
+                            text: '¡No deje campos vacios!',
                         })
+                    })
                 }
             })
 
 
         },
-        enviarEmail: function() {
+        enviarEmail:function(){
             var user = {
-                nombre: this.nombre,
-                apellidop: this.apellidop,
-                apellidom: this.apellidom,
-                tratamiento: this.tratamiento,
-                email: this.email,
-                usuario: this.usuario,
-                password: this.password,
+                nombre : this.nombre,
+                apellidop : this.apellidop,
+                apellidom : this.apellidom,
+                tratamiento : this.tratamiento,
+                email : this.email,
+                usuario : this.usuario,
+                password : this.password,
             };
-            this.$http.post(UserName, user)
-                .then(function() {
-                    console.log(json);
-                }).catch(function(json) {
-                    console.log(json);
-                })
+            this.$http.post(UserName,user)
+            .then(function(){
+
+            }).catch(function(json){
+                console.log(json);
+            })
         },
-        salir: function() {
+        salir:function(){
             $('#agregar_user').modal('hide');
-            this.cedula = "";
-            this.id_rol = "";
-            this.nombre = "";
-            this.apellidop = "";
-            this.apellidom = "";
-            this.nivelestudio = "";
-            this.profesion = "";
-            this.tratamiento = "";
-            this.activo = "";
-            this.foto = "";
-            this.usuario = "";
-            this.password = "";
-            this.email = "";
-            this.i_cedula = "";
-            this.editarInfo = false;
+                this.cedula="";
+                this.id_rol = "";
+                this.nombre = "";
+                this.apellidop = "";
+                this.apellidom = "";
+                this.nivelestudio = "";
+                this.profesion = "";
+                this.tratamiento = "";
+                this.activo = "";
+                this.foto = "";
+                this.usuario = "";
+                this.password = "";
+                this.email = "";
+                this.i_cedula = "";
+                this.editarInfo = false;
         }
     },
 })
