08d06fccc4b28c56aa5077a21c6e1ce5cfdc901a
Jesús Canul <51716401+JesusAlbertoCanulAcosta@users.noreply.github.com>
2021-09-21 23:23:57 -0500

    Corrección de Titulo Docente-inicio
    se corrigió el nombre del titulo de la página de inicio del docente.

--- resources/views/layouts/masterDocente.blade.php
+++ resources/views/layouts/masterDocente.blade.php
@@ -6,7 +6,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href="material/img/apple-icon.png">
     <link rel="icon" type="image/png" href="material/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Director de Carreras</title>
+    <title>Docente</title>
     {{-- TOKEN PARA CAMBIOS --}}
     <meta name="token" id="token" value="{{ csrf_token() }}">
     {{-- META PARA RUTA DINAMICA --}}
